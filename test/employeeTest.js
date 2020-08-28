44c37ac40bc14339ed217e6f5666ac6d95a640cc
Lester Lin <LINLE5@OOCL.COM>
2020-08-28 17:56:57 +0800

    add employee case2 of throw type exception..

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -7,8 +7,14 @@ test('employee case1', t=> {
     t.is('name: Lester (engineer)', result);
 });
 
-test('employee case2', t=> {
+test('employee case2 of throw type exception.', t=> {
     var employee = new Employee('Lester' ,'engineer');
     const result = 'name: ' + employee.toString();
     t.is('name: Lester (engineer)', result);
+    try {
+        employee.validateType(employee._type);
+      }
+      catch (e) {
+        t.is(e.message, `Employee cannot be of type ${type}`);
+      }
 });
